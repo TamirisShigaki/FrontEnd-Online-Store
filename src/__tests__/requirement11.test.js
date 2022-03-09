96776384a26ec3334874b45d2f85bf170a965a23
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-03-09 17:28:46 -0300

    Initial commit

---
+++ src/__tests__/requirement11.test.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import App from '../App';
+import mockedQueryResult from '../__mocks__/query';
+import mockFetch from '../__mocks__/mockFetch';
+
+describe(`11 - Avalie e comente acerca de um produto em sua tela de exibição detalhada`, () => {
+  afterEach(() => {
+    global.fetch.mockClear();
+  });
+
+  it('Avalia todo o campo de avaliação na sua tela de detalhes', async () => {
+    const evaluationEmail = `teste@trybe.com`;
+    const evaluationContent = `Esta é uma avaliação sobre o produto realizada na tela de detalhe.`;
+    jest.spyOn(global, 'fetch').mockImplementation(mockFetch)
+    
+    render(<App />);
+    await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+    
+    fireEvent.click(screen.getAllByTestId('category')[0]);
+    await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(2));
+    
+    fireEvent.click(screen.getAllByTestId('product-detail-link')[0]);
+    await waitFor(
+      () => expect(screen.getByTestId('product-detail-name')).toHaveTextContent(
+        mockedQueryResult.results[0].title,
+      ),
+    );
+
+    fireEvent.change(
+      screen.getByTestId('product-detail-email'),
+      { target: { value: evaluationEmail } },
+    );
+    expect(screen.getByTestId('product-detail-email')).toHaveValue(evaluationEmail);
+    
+    fireEvent.change(
+      screen.getByTestId('product-detail-evaluation'),
+      { target: { value: evaluationContent } },
+    );
+    expect(screen.getByTestId('product-detail-evaluation')).toHaveValue(evaluationContent);
+
+    for (let index = 1; index <= 5; index += 1) {
+      expect(screen.getByTestId(`${index}-rating`)).toBeVisible();
+    };
+    fireEvent.click(screen.getByTestId('3-rating'));
+
+    expect(screen.getByTestId('submit-review-btn')).toBeVisible();
+    fireEvent.click(screen.getByTestId('submit-review-btn'));
+    
+    await waitFor(
+      () => {
+        expect(screen.getByText(evaluationEmail)).toBeVisible();
+        expect(screen.getByText(evaluationContent)).toBeVisible();
+      }
+    );
+
+  });
+
+  it('Avalia se a avaliação continua após recarregar a pagina', async () => {
+    const evaluationEmail = `teste@trybe.com`;
+    const evaluationContent = "Esta é uma avaliação sobre o produto realizada na tela de detalhe.";
+    jest.spyOn(global, 'fetch').mockImplementation(mockFetch)
+    render(<App />);
+    await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+
+    expect(screen.getByText(evaluationEmail)).toBeVisible();
+    expect(screen.getByText(evaluationContent)).toBeVisible();
+  });
+});
